50cd3678a2606d58f55c1220b35c8c932ec4c910
acrolink <contact@acro.link>
2023-06-13 11:57:23 +0300

    Update s3-zip.js
    added archiveStreamToMemory

--- s3-zip.js
+++ s3-zip.js
@@ -1,5 +1,6 @@
 const s3Files = require('s3-files')
 const archiver = require('archiver')
+var Writable = require('stream').Writable;
 
 const s3Zip = {}
 module.exports = s3Zip
@@ -30,12 +31,80 @@ s3Zip.archive = function (opts, folder, filesS3, filesZip) {
 
   const preserveFolderStructure = opts.preserveFolderStructure === true || filesZip
   const fileStream = s3Files.createFileStream(keyStream, preserveFolderStructure)
-  const archive = self.archiveStream(fileStream, filesS3, filesZip)
-
+  //const archive = self.archiveStreamToFile(fileStream, filesS3, filesZip)
+  const archive = self.archiveStreamToMemory(fileStream, filesS3, filesZip)
   return archive
 }
 
-s3Zip.archiveStream = function (stream, filesS3, filesZip) {
+
+s3Zip.archiveStreamToMemory = function (stream, filesS3, filesZip) {
+
+  return new Promise((resolve, reject) => {
+
+    const buffs = []
+
+    const converter = new Writable()
+
+    converter._write = (chunk, encoding, cb) => {
+      buffs.push(chunk)
+      process.nextTick(cb)
+    }
+
+    converter.on('finish', () => {
+      resolve(Buffer.concat(buffs))
+    })
+
+    const self = this
+    const folder = this.folder || ''
+    if (this.registerFormat) {
+      archiver.registerFormat(this.registerFormat, this.formatModule)
+    }
+    const archive = archiver(this.format || 'zip', this.archiverOpts || {})
+    archive.on('error', function (err) {
+      self.debug && console.log('archive error', err)
+    })
+
+    archive.pipe(converter)
+
+    stream
+      .on('data', function (file) {
+        if (file.path[file.path.length - 1] === '/') {
+          self.debug && console.log('don\'t append to zip', file.path)
+          return
+        }
+        let fname
+        if (filesZip) {
+          // Place files_s3[i] into the archive as files_zip[i]
+          const i = filesS3.indexOf(file.path.startsWith(folder) ? file.path.substr(folder.length) : file.path)
+          fname = (i >= 0 && i < filesZip.length) ? filesZip[i] : file.path
+          filesS3[i] = ''
+        } else {
+          // Just use the S3 file name
+          fname = file.path
+        }
+        const entryData = typeof fname === 'object' ? fname : { name: fname }
+        self.debug && console.log('append to zip', fname)
+        if (file.data.length === 0) {
+          archive.append('', entryData)
+        } else {
+          archive.append(file.data, entryData)
+        }
+      })
+      .on('end', function () {
+        self.debug && console.log('end -> finalize')
+        archive.finalize()
+      })
+      .on('error', function (err) {
+        archive.emit('error', err)
+      })
+
+    return archive
+  });
+
+}
+
+
+s3Zip.archiveStreamToFile = function (stream, filesS3, filesZip) {
   const self = this
   const folder = this.folder || ''
   if (this.registerFormat) {
